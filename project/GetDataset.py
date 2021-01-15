e09b1d9ea9bdcf8cc3454f60eb309504ec72938b
Tanvy <dggaltanvir@gmail.com>
2021-01-14 21:58:49 -0500

    Signed-off-by: Tanvy <dggaltanvir@gmail.com>

--- project/GetDataset.py
+++ project/GetDataset.py
@@ -10,6 +10,11 @@ import numpy as np
 
 class GetData:
     dataList = []
+    X        = []
+    Y        = []
+    
+    X_df     = ''
+    Y_df     = ''
     
     def __init__(self, path):
         self.readData(path)
@@ -18,9 +23,17 @@ class GetData:
         with open(path, 'r',  encoding="utf8") as f:
             for line in f.readlines():
                 if line.strip('\n') != "":
-                    print(">> ", line)
-    
+                    for word in line.split('.'):
+                        if len(word) > 1:
+                            self.dataList.append(word.strip())
+                            
     def convertToDataframe(self):
-        pass
-    
+        for data in self.dataList:
+            statement = data.split(" ")
+            for i in range(len(statement)):
+                if i < len(statement)-4:
+                    self.X.append(' '.join(statement[i:i+4]))
+                    self.Y.append(statement[i+4])
+        
 gt = GetData('../datasets/AI_dataset.txt')
+gt.convertToDataframe()
--- project/Index.py
+++ project/Index.py
@@ -5,9 +5,31 @@ Created on Tue Nov 10 22:56:40 2020
 @author: Tanvy
 """
 
+import pandas as pd
+import numpy as np
+
+from GetDataset import GetData
+from Process import ProcessData
 
 def main():
-    pass
+    gt = GetData('../datasets/AI_dataset.txt')
+    gt.convertToDataframe()
+    
+    X  = gt.X
+    Y  = gt.Y
+    
+    dfX = pd.DataFrame(X, columns=['review'])
+    
+    processData = ProcessData(dfX)
+    processData.connvertLowerCase()
+    processData.removeUnwantedItems()
+    processData.lemmatization()
+    processData.tokenizing()
+    
+    X_TextSeq = processData.txtSeq
+    
+    print(X_TextSeq)
+    
 
 if __name__ == '__main__':
     main()
